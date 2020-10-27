13de64d94796b07c5d5a62a276716c5956707427
颜培攀 <yanpeipan@xiaomi.com>
2020-10-27 20:09:07 +0800

    weak02

--- Week_01/Hello.java
+++ Week_01/Hello.java
@@ -1,11 +1,22 @@
+import java.lang.management.MemoryUsage;
+
 public class Hello {
 
-    private final static boolean btrue = true;
+    private static boolean btrue = true;
 
-    public static void main(String[] args) {
+    @Deprecated
+    private static class World {
+        private World() {
+            System.out.println(btrue);
+        }
+    }
+
+    public static void main(String[] args) throws Error {
+        System.out.println(btrue = backspaceCompare("xywrrmp",
+                "xywrrmu#p"));
+
         int ia = -10, ib = 1000;
         double d1 = 2L;
-        final boolean bfalse = false;
 
         try {
             d1 += add(ia, ib);
@@ -15,9 +26,57 @@ public class Hello {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+
+
+        throw new IllegalAccessError();
     }
 
     public static long add(int a, int b) {
         return a + b;
+    }
+
+    public static boolean backspaceCompare(String S, String T) {
+        int j = T.length() - 1, i = S.length() - 1;
+        int skipJ = 0, skipI = 0;
+        while (i >= 0) {
+
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    i--;
+                    skipI++;
+                } else if (skipI > 0) {
+                    i--;
+                    skipI--;
+                } else {
+                    break;
+                }
+            }
+
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    j--;
+                    skipJ++;
+                } else if (skipJ > 0) {
+                    j--;
+                    skipJ--;
+                } else {
+                    break;
+                }
+            }
+
+            if (i >= 0 && j >= 0) {
+                if (S.charAt(i) != T.charAt(j)) {
+                    return false;
+                }
+            }
+
+            i--;
+            j--;
+        }
+        if (i == j) {
+            return true;
+        }
+
+        return false;
     }
 }
--- Week_02/README.md
+++ Week_02/README.md
@@ -1,1 +1,11 @@
 学习笔记
+
+java -XX:+PrintGCDetails GCLogAnalysis
+
+java -Xloggc:gc.demo.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps GCLogAnalysis
+
+java -XX:+UseSerialGC -Xms512m -Xmx512m -Xloggc:gc.demo.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps GCLogAnalysis
+
+java -XX:+UseParallelGC -Xms512m -Xmx512m -Xloggc:gc.demo.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps GCLogAnalysis
+
+java -XX:+UseConcMarkSweepGC -Xms512m -Xmx512m -Xloggc:gc.demo.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps GCLogAnalysis
